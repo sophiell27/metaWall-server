11f928724b0367cbfc88d8d7f72e2f12fcdcc254
sophiee <105938870+sophiell27@users.noreply.github.com>
2024-05-26 13:51:19 +0800

    chore: add cors

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors({ origin: '*' }));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
