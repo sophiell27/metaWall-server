226dfe77fe54197c1dd236a5de8e38aefdb6e7c0
Sophie Lam <sophie.lam0207@gmail.com>
2025-05-24 09:24:58 +0800

    feat: add /ping server wake-up route to index router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (_, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/ping', function (_, res) {
+  res.send('pong');
 });
 
 module.exports = router;
